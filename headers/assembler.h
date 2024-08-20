c57660f14d4f12ad5cbef36891c9beaa0137ce59
Itai-Arnon <itai.arnon1@gmail.com>
2024-08-20 15:34:54 +0300

    update

--- headers/assembler.h
+++ headers/assembler.h
@@ -7,16 +7,27 @@
 #include "first_pass.h"
 
 /*sends files to pre processor and returns an am file*/
-void manage_files(int  ,char ** , macro_table_t*, symbol_table_t*, word_table_t *,word_table_t *);
+void manage_files(int, char **, macro_table_t *, symbol_table_t *, word_table_t *, word_table_t *);
 
 void move_one_directory_up(char *path);
+
 FILE *initSourceFiles(int _argc, char **_argv, FILE *fptr, int index);
+
 FILE *initDestinationPointer(FILE *fptr, char *filename, char mode[]);
-char* addExtension(char *file_name ,char* ext);
-void report_error(char *, int , file_t , err_type_t , int );
-		void freeWordTable(word_table_t *wordTable);
+
+char *addExtension(char *file_name, char *ext);
+
+void report_error(char *, int, file_t, err_type_t, int);
+
+void freeWordTable(word_table_t *wordTable);
+
 void freeWordTable(word_table_t *table);
+
 void freeSymbolTable(symbol_table_t *symbolTable);
+
 void freeMacroTable(macro_table_t *macroTable);
-void freeAllTables(macro_table_t *macroTable, symbol_table_t *symbolTable, word_table_t *wordTable, word_table_t *dataTable);
+
+void
+freeAllTables(macro_table_t *macroTable, symbol_table_t *symbolTable, word_table_t *wordTable, word_table_t *dataTable);
+
 #endif /*M14_ASSEMBLER_H*/
